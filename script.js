a58a67d5e6378de7f25e0f8f66d7dda9272b7011
Richard Remley <5rremley@gmail.com>
2022-07-05 14:44:13 -0500

    Group 2 Richard Remley

---
+++ script.js
@@ -0,0 +1,47 @@
+let button = document.querySelector("#search");
+let input = document.querySelector("#search-input");
+let section = document.querySelector(`.movie-list`);
+// const renderList = (movies) => {
+//   movies.map((currentMovie) => {
+//     let movieName = currentMovie.title;
+//     console.log(movieName);
+//     // let movieImage = currentMovie.poster_path;
+//     let movieTitle = document.createElement("h1");
+//     // let movieImage2 = document.createElement("img");
+//     movieTitle.innerText = `${movieName}`;
+//     // movieImage2.setAttribute("src", `${IMAGE_BASE_PATH}${movieImage}`);
+
+//     section.appendChild(movieTitle);
+//     section.appendChild(movieImage2);
+//   });
+// };
+
+button.addEventListener("click", async () => {
+  let movieChoice = input.value;
+  const response = await axios.get(
+    `https://anapioficeandfire.com/api/characters/${movieChoice}`
+  );
+  console.log(response);
+
+  let nameOptions = response.data.name;
+  let result = document.createElement("h1");
+  result.innerHTML = `Name: ${nameOptions}`;
+  section.appendChild(result);
+
+  let genderOptions = response.data.gender;
+  let gender = document.createElement("h1");
+  gender.innerHTML = `Sex: ${genderOptions}`;
+  section.appendChild(gender);
+
+  let diedOptions = response.data.died;
+  let died = document.createElement("h1");
+  died.innerHTML = `Year of Death: ${diedOptions}`;
+  section.appendChild(died);
+
+  let allegiancesOptions = response.data.allegiances;
+  let allegiances = document.createElement("h1");
+  allegiances.innerHTML = `Allegiance: ${allegiancesOptions}`;
+  section.appendChild(allegiances);
+  //   renderList(movieOptions);
+  //   console.log(movieOptions);
+});
